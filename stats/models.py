bc75da088cd16d73d97ae1b781c55b17a87c1102
RANDRIANAMBININA Anicet <1086@production.local>
2024-09-20 16:38:34 +0300

    models stats

--- stats/models.py
+++ stats/models.py
@@ -8,6 +8,7 @@ class Stat(models.Model):
     value = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=100, blank=True, null=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                             blank=True)
     description = models.TextField(blank=True, null=True)
     is_admin_generated = models.BooleanField(default=False)
--- stats/views.py
+++ stats/views.py
@@ -1,19 +1,20 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
+
+from core.permissions import IsAdminUser
 from .models import Stat
 from .serializers import StatSerializer
 
 class StatViewSet(viewsets.ModelViewSet):
     queryset = Stat.objects.all()
     serializer_class = StatSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         if self.request.user.is_admin:
             return Stat.objects.all()
 
         return Stat.objects.filter(user=self.request.user)
-
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        user_id = self.request.data.get('user')
+        serializer.save(user_id=user_id)
