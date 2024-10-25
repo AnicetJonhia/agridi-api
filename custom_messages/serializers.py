459c8aa9d550d243dbf6fa9aeb43056557feef79
RANDRIANAMBININA Anicet <1086@production.local>
2024-10-25 14:52:26 +0300

    messages

--- custom_messages/serializers.py
+++ custom_messages/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-
 from users.models import User
 from .models import Message, Group
+
+class UserSerializerForMessage(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
 
 class GroupSerializer(serializers.ModelSerializer):
     members = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
@@ -12,12 +20,11 @@ class GroupSerializer(serializers.ModelSerializer):
         read_only_fields = ['owner']
 
 class MessageSerializer(serializers.ModelSerializer):
-    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all(),
-                                               required=False)
-
-    receiver = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all(), required=False)
+    receiver = UserSerializerForMessage(read_only=True)
+    sender = UserSerializerForMessage(read_only=True)
 
     class Meta:
         model = Message
         fields = '__all__'
-        read_only_fields = ['sender','timestamp']
+        read_only_fields = ['sender', 'timestamp', 'receiver']
