6a0368b23e7622d430e7075eb625fb4e7e1dde9e
AnicetJonhia <anicet22.aps2a@gmail.com>
2024-11-03 22:10:50 +0300

    consumer

--- custom_messages/consumers.py
+++ custom_messages/consumers.py
@@ -3,44 +3,34 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
-
-        # Rejoindre le groupe
+        self.group_name = self.scope['url_route']['kwargs']['group_name']
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Quitter le groupe
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
-        message = data['message']
-        sender = self.scope['user'].username
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-        # Envoyer le message au groupe
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'sender': sender,
+                'message': message
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-        sender = event['sender']
 
-        # Envoyer le message au WebSocket
         await self.send(text_data=json.dumps({
-            'message': message,
-            'sender': sender,
+            'message': message
         }))
