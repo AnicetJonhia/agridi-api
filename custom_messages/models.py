9889dc1cf334eed98026ac41e507fdb8a98765ea
AnicetJonhia <anicet22.aps2a@gmail.com>
2024-11-09 09:26:43 +0300

    models messagees

--- custom_messages/models.py
+++ custom_messages/models.py
@@ -14,7 +14,7 @@ class Group(models.Model):
 
 
 class Message(models.Model):
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     file = models.FileField(upload_to='custom_messages/files/', blank=True, null=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages', blank=True, null=True)
--- custom_messages/views.py
+++ custom_messages/views.py
@@ -120,23 +120,25 @@ class MessageViewSet(viewsets.ModelViewSet):
         serializer = MessageSerializer(unique_messages, many=True, context={'request': request})
         return Response(serializer.data)
 
-
-
     @action(detail=True, methods=['get'], permission_classes=[IsAuthenticated])
-    def chat_history(self, request, pk=None):
+    def chat_history(self, request, pk=None, type=None):
         """Retourne l'historique des messages entre deux utilisateurs ou pour un groupe spécifique."""
-        try:
-            receiver = User.objects.get(pk=pk)
-            messages = Message.objects.filter(
-                models.Q(sender=request.user, receiver=receiver) |
-                models.Q(sender=receiver, receiver=request.user)
-            ).order_by('timestamp')
-            if not messages.exists():
-                logger.debug(f"No messages found between users {request.user.id} and {receiver.id}")
-            serializer = MessageSerializer(messages, many=True, context={'request': request})
-            return Response(serializer.data)
-        except User.DoesNotExist:
-            logger.debug(f"User with id {pk} does not exist")
+        if type == 'private':
+            try:
+                receiver = User.objects.get(pk=pk)
+                messages = Message.objects.filter(
+                    models.Q(sender=request.user, receiver=receiver) |
+                    models.Q(sender=receiver, receiver=request.user)
+                ).order_by('timestamp')
+                if not messages.exists():
+                    logger.debug(f"No messages found between users {request.user.id} and {receiver.id}")
+                serializer = MessageSerializer(messages, many=True, context={'request': request})
+                return Response(serializer.data)
+            except User.DoesNotExist:
+                logger.debug(f"User with id {pk} does not exist")
+                return Response({"detail": "Aucun utilisateur correspondant trouvé."},
+                                status=status.HTTP_404_NOT_FOUND)
+        elif type == 'group':
             try:
                 group = Group.objects.get(pk=pk)
                 messages = Message.objects.filter(group=group).order_by('timestamp')
@@ -146,8 +148,11 @@ class MessageViewSet(viewsets.ModelViewSet):
                 return Response(serializer.data)
             except Group.DoesNotExist:
                 logger.debug(f"Group with id {pk} does not exist")
-                return Response({"detail": "Aucun utilisateur ou groupe correspondant trouvé."},
+                return Response({"detail": "Aucun groupe correspondant trouvé."},
                                 status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({"detail": "Type invalide. Utilisez 'user' ou 'group'."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
     @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated], parser_classes=[MultiPartParser])
